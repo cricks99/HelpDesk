a3bb737ee1a2f991c1f08650683dd876a3a08157
BChunn39 <113707879+BChunn39@users.noreply.github.com>
2023-02-22 21:44:22 -0500

    Update to fix database

---
+++ HelpDeskAPI/Models/Ticket.cs
@@ -0,0 +1,13 @@
+namespace HelpDeskAPI.Models
+{
+  public class Ticket
+  {
+    public int Id { get; set; }
+    public int OpenUserId { get; set; }
+    public string Description { get; set; }
+    public string? Resolution { get; set; }
+    public int? ClosingUserId { get; set; }
+    public bool IsClosed { get; set; }
+    public virtual List<Favorite> Favorites { get; set; }
+  }
+}
